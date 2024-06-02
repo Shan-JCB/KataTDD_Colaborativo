2a3dcc7949f7af6ea01e7601169da510df5cdf8c
Shan-JCB <71997263@continental.edu.pe>
2024-06-02 11:10:40 -0500

    Caso 2: Calcular promedio con un conjunto de un elemento (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -4,14 +4,10 @@ class Conjunto:
         self.__pesos = pesos
 
     def promedio(self):
-        if not self.__conjunto:  # Verifica si el conjunto está vacío
+
+        if len(self.__conjunto) == 1:
+            return (self.__conjunto[0])
+
+        else:
             return None
-        if not self.__pesos:  # Si no hay pesos, calcula el promedio simple
-            return sum(self.__conjunto) / len(self.__conjunto)
 
-        total_peso = sum(self.__pesos)
-        if total_peso == 0:
-            return None  # Evitar división por cero
-
-        promedio_ponderado = sum(x * w for x, w in zip(self.__conjunto, self.__pesos)) / total_peso
-        return round(promedio_ponderado, 2)
