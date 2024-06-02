6384d68295682633fe13e79b53f3f4c1770bbf49
Shan-JCB <71997263@continental.edu.pe>
2024-06-02 10:57:52 -0500

    Caso 2: Calcular promedio con un conjunto de un elemento (Rojo)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,18 +1,17 @@
 class Conjunto:
-    def _init_(self, conjunto, pesos=None):
+    def __init__(self, conjunto, pesos=None):
         self.__conjunto = conjunto
         self.__pesos = pesos
 
     def promedio(self):
-
         if not self.__conjunto:  # Verifica si el conjunto está vacío
             return None
         if not self.__pesos:  # Si no hay pesos, calcula el promedio simple
-            return sum(self._conjunto) / len(self._conjunto)
+            return sum(self.__conjunto) / len(self.__conjunto)
 
         total_peso = sum(self.__pesos)
         if total_peso == 0:
             return None  # Evitar división por cero
 
-        promedio_ponderado = sum(x * w for x, w in zip(self._conjunto, self._pesos)) / total_peso
+        promedio_ponderado = sum(x * w for x, w in zip(self.__conjunto, self.__pesos)) / total_peso
         return round(promedio_ponderado, 2)
--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,9 +1,13 @@
 import unittest
 from src.logica.Conjunto import Conjunto
 class TestConjunto(unittest.TestCase):
-    def test_conjunto_vacio_retornaNone(self):
-        conjunto = Conjunto([])
-        self.assertIsNone(conjunto.promedio())
+        def test_conjunto_vacio_retornaNone(self):
+            conjunto = Conjunto([])
+            self.assertIsNone(conjunto.promedio())
+
+        def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+            conjunto = Conjunto([5], [2])
+            self.assertEqual(5, conjunto.promedio())
 
 if __name__ == '__main__':
     unittest.main()
